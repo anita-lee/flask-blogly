eaadd045d4abdf747f187d8251e3a3fc41db0db6
Anita Lee <anita.lee@gmail.com>
2022-03-30 18:31:16 -0400

    finished html files

--- app.py
+++ app.py
@@ -1,12 +1,27 @@
 """Blogly application."""
 
 from flask import Flask
-from models import db, connect_db
+from models import db, connect_db, User
+from flask_debugtoolbar import DebugToolbarExtension
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///blogly'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
 
+# RESPONSES_KEY = "responses"
+# app.config['SECRET_KEY'] = "never-tell!"
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+debug = DebugToolbarExtension(app)
+
+
+
 connect_db(app)
 db.create_all()
+
+@app.get("/")
+def list_users():
+    """List users and show add form."""
+
+    users = User.query.all()
+    return render_template("list.html", users=users)
