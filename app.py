294b1e09ae78ef6aa4b15f5b5b4920f960f7eaad
Anita Lee <anita.lee@gmail.com>
2022-03-30 21:09:22 -0400

    completed blogly day one, tests not started

--- app.py
+++ app.py
@@ -17,11 +17,10 @@ debug = DebugToolbarExtension(app)
 connect_db(app)
 db.create_all()
 
+
 @app.get("/")
 def list_users():
     """List users and show add form."""
-
-
     return redirect("/users")
 
 @app.get("/users")
@@ -43,39 +42,62 @@ def add_new_user():
     """Adds new User to db"""
 
     #retrieve form data here
+    first_name = request.form['first_name']
+    last_name = request.form['last_name']
+    image_url = request.form['image_url']
+
+    user = User(first_name=first_name, last_name=last_name, image_url=image_url)
+    db.session.add(user)
+    db.session.commit()
 
     return redirect("/")
 
-app.get("/user/<user_id>")
+@app.get("/users/<int:user_id>")
 def show_user_by_id(user_id):
     """Display user by id."""
 
     #retrieve data for specified user_id
     #pass data to template.
 
-    return render_template("user_detail.html")
+    user = User.query.get_or_404(user_id)
+    print("USER=", user)
 
-app.get("/user/<user_id>/edit")
+    return render_template("user_detail.html", user=user)
+
+@app.get("/users/<int:user_id>/edit")
 def show_edit_user(user_id):
     """Display edit user page."""
 
     #retrieve data for specified user_id
     #pass data to template.
+    user = User.query.get_or_404(user_id)
 
-    return render_template("user_edit.html")
+    return render_template("user_edit.html", user=user)
 
-app.post("/users/<user_id>/edit")
+@app.post("/users/<int:user_id>/edit")
 def edit_user(user_id):
     """Process edit form, redirect to root"""
 
     #process edit,
+    user = User.query.get_or_404(user_id)
+
+    user.first_name = request.form['first_name']
+    user.last_name = request.form['last_name']
+    user.image_url = request.form['image_url']
+
+    db.session.commit()
 
     return redirect("/")
 
-app.post("/user/<user_id>/delete")
+@app.post("/users/<int:user_id>/delete")
 def delete_user(user_id):
     """Delete user from db"""
 
     #"delete" user from db.
+    user = User.query.get_or_404(user_id)
+
+    # user.query.delete()
+    db.session.delete(user)
+    db.session.commit()
 
     return redirect("/")
